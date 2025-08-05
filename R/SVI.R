a5d23633277571d6e419770fc39f22874c304ef7
chbsila <137139630+chbsila@users.noreply.github.com>
2025-08-05 12:23:44 -0400

    Update SVI.R

--- R/SVI.R
+++ R/SVI.R
@@ -6,3 +6,10 @@
 # Authors: Chadi Bsila, Kevin Wang, Annie Tang
 # Supported by: DRI 2025
 # ------------------------------------------------------------
+
+# ============================================================
+# Set Working Directory to Existing DRI Folder
+# ============================================================
+save_dir <- "~/Desktop/Other Methods"   
+setwd(save_dir)
+cat("Saving all results to:", getwd(), "\n")
