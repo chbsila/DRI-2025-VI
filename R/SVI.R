e87214c0ed980395b175bd32a68f18f072a067a3
chbsila <137139630+chbsila@users.noreply.github.com>
2025-08-05 12:22:46 -0400

    Update SVI.R

---
+++ R/SVI.R
@@ -0,0 +1,8 @@
+# ------------------------------------------------------------
+# Stochastic Variational Inference (SVI) Algorithm
+# for Laplace Spike-and-Slab in High-Dimensional Linear Regression
+# with Rényi Divergence and Monte Carlo Rényi Lower Bound
+#
+# Authors: Chadi Bsila, Kevin Wang, Annie Tang
+# Supported by: DRI 2025
+# ------------------------------------------------------------
